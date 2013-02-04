f0ff8c2587fc26a9b17895838a37c82f8e7cfb85
Tim Valenta <tonightslastsong@gmail.com>
2013-02-04 00:00:49 -0700

    Add name to wallpaper upload url

--- wtds/wallpapers/urls.py
+++ wtds/wallpapers/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from .views import WallpaperCreateView
 
 urlpatterns = patterns('',
-    url(r'^new/$', WallpaperCreateView.as_view()),
+    url(r'^new/$', WallpaperCreateView.as_view(), name='upload'),
 )
