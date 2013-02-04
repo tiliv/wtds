3c8735ca869cc6ef7845436b2844713373aacbcb
Tim Valenta <tonightslastsong@gmail.com>
2013-02-03 23:33:55 -0700

    Clean up creation url

--- wtds/wallpapers/urls.py
+++ wtds/wallpapers/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from .views import WallpaperCreateView
 
 urlpatterns = patterns('',
-    url(r'^$', WallpaperCreateView.as_view()),
+    url(r'^new/$', WallpaperCreateView.as_view()),
 )
