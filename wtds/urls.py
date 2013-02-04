123253dfd9529ec4c567caf88e81d81424fb3a70
Tim Valenta <tonightslastsong@gmail.com>
2013-02-04 01:58:44 -0700

    Adds debug MEDIA_URL file serving

--- wtds/urls.py
+++ wtds/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 # from django.contrib import admin
 
 from .views import HomeView
@@ -13,3 +14,10 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^{}(?P<path>.*)$'.format(settings.MEDIA_URL.lstrip('/')), 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        })
+    )
