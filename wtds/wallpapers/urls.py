83a187953a976dc548623e69419ffc3afcafa389
Tim Valenta <tonightslastsong@gmail.com>
2013-02-04 02:00:48 -0700

    Add basic wallpaper detail view

--- wtds/wallpapers/urls.py
+++ wtds/wallpapers/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
 
-from .views import WallpaperCreateView
+from .views import WallpaperCreateView, WallpaperDetailView
 
 urlpatterns = patterns('',
     url(r'^new/$', WallpaperCreateView.as_view(), name='upload'),
+    url(r'^view/(?P<pk>\d+)/$', WallpaperDetailView.as_view(), name='view'),
 )
--- wtds/wallpapers/views.py
+++ wtds/wallpapers/views.py
@@ -1,9 +1,24 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
+from django.http import HttpResponseRedirect
 
 from .models import Wallpaper
 from .forms import CreateForm
 
-class WallpaperCreateView(CreateView):
+class WallpaperMixin(object):
     model = Wallpaper
-    
+
+class WallpaperCreateView(WallpaperMixin, CreateView):
     form_class = CreateForm
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.uploader = self.request.user
+        self.object.save()
+
+        # Saves the tags
+        form.save_m2m()
+
+        return HttpResponseRedirect(self.get_success_url())
+
+class WallpaperDetailView(WallpaperMixin, DetailView):
+    pass
