dcb8d72c499986e8cc7c87ec35530ad961554847
Tim Valenta <tonightslastsong@gmail.com>
2013-02-05 01:30:33 -0700

    Adds basic list view with tag filtering.

--- wtds/wallpapers/urls.py
+++ wtds/wallpapers/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import patterns, url, include
 
-from .views import WallpaperCreateView, WallpaperDetailView, WallpaperDeleteView
+from .views import WallpaperCreateView, WallpaperDetailView, WallpaperDeleteView, WallpaperListView
 
 urlpatterns = patterns('',
     url(r'^new/$', WallpaperCreateView.as_view(), name='upload'),
+    url(r'^list/', include(patterns('',
+        url(r'^$', WallpaperListView.as_view(view_all=True), name='list'),
+        url(r'^(?P<slug>[\w-]+)/$', WallpaperListView.as_view(), name='list'),
+        # url(r'^(?P<slugs>.*)$', WallpaperListView.as_view(), name='list'),
+    ))),
     url(r'^(?P<pk>\d+)/', include(patterns('',
         url(r'^$', WallpaperDetailView.as_view(), name='view'),
         url(r'^delete/$', WallpaperDeleteView.as_view(), name='delete'),
--- wtds/wallpapers/views.py
+++ wtds/wallpapers/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import CreateView, DetailView, DeleteView
+from django.views.generic import CreateView, DetailView, DeleteView, ListView
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse_lazy
+from taggit.models import Tag
+from taggit.utils import parse_tags
 
 from .models import Wallpaper
 from .forms import CreateForm
@@ -26,3 +28,30 @@ class WallpaperDetailView(WallpaperMixin, DetailView):
 
 class WallpaperDeleteView(WallpaperMixin, DeleteView):
     success_url = reverse_lazy('home')
+
+class WallpaperListView(WallpaperMixin, ListView):
+    view_all = False
+    
+    queryset = Wallpaper.objects.all()
+    
+    _tags = None
+
+    def get_filter_tags(self):
+        if self._tags is None:
+            if 'slug' in self.kwargs:
+                self._tags = Tag.objects.filter(slug=self.kwargs['slug']) # yields queryset
+            else:
+                self._tags = Tag.objects.filter(slug__in=self.request.GET.getlist('tag'))
+        return self._tags
+
+    # TODO: Sorting
+    def get_queryset(self):
+        return self.queryset.filter(tags__in=self.get_filter_tags()).distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super(WallpaperListView, self).get_context_data(**kwargs)
+        context.update({
+            'tags': self.get_filter_tags(),
+        })
+        return context
+        
