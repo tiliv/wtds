d6015e3c0d95b1d8b9a62bce559c1bec2548237a
Tim Valenta <tonightslastsong@gmail.com>
2013-02-06 23:43:21 -0700

    Adds thumbnail widget image widget
    The widget is based on the one provided by sorl-thumbnails, but removes some markup that made it behave strangely in non-admin settings.

--- wtds/wallpapers/widgets.py
+++ wtds/wallpapers/widgets.py
@@ -3,6 +3,9 @@ from operator import attrgetter
 
 from django import forms
 from django.utils.safestring import mark_safe
+from django.template.loader import render_to_string
+
+from sorl.thumbnail.shortcuts import get_thumbnail
 
 class DragAndDropImageProcesserWidget(forms.FileInput):
     class Media:
@@ -11,14 +14,11 @@ class DragAndDropImageProcesserWidget(forms.FileInput):
 
     def render(self, name, value, attrs=None):
         s = super(DragAndDropImageProcesserWidget, self).render(name, value, attrs)
-
-        s = mark_safe(s + """
-            <div id="hover-indicator"></div>
-            <div id="preview-stage">
-                <div id="size-readout"></div>
-            </div>
-        """)
-
+        s = mark_safe(s + u""
+            u'<div id="hover-indicator"></div>'
+            u'<div id="preview-stage">'
+                u'<div id="size-readout"></div>'
+            u'</div>')
         return s
 
 
@@ -42,3 +42,26 @@ class TagListInput(forms.TextInput):
         options = json.dumps(self.tagsInput_options)
         return mark_safe(s + js.format(tagsInput_options=options, **attrs))
 
+
+class ClearableThumbnailImageWidget(forms.ClearableFileInput):
+    """
+    Subtle modification to the style of implementation found in ``sorl.thumbnail.admin.current``.
+    The original doesn't work very well with ``form.as_p`` and such, due to the way it is designed
+    to work only with the ``contrib.admin`` interface.
+    
+    """
+
+    template_with_initial = u'%(clear_template)s %(input_text)s: %(input)s'
+
+    def render(self, name, value, attrs=None):
+        output = super(ClearableThumbnailImageWidget, self).render(name, value, attrs)
+        if value and hasattr(value, 'url'):
+            try:
+                thumbnail = get_thumbnail(value, '280x188', crop="center")
+            except Exception:
+                pass
+            else:
+                output = (u'<a class="thumbnail tile" target="_blank" href="%s">'
+                    u'<img src="%s" width="140" height="94" /></a>') % (value.url, thumbnail.url) \
+                    + output
+        return mark_safe(output)
