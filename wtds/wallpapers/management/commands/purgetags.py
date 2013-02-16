e3206fbba096f627360459109538a763801675f0
Tim Valenta <tonightslastsong@gmail.com>
2013-02-16 01:29:15 -0700

    Changes purgetags parent class
    To remain consistent with the provided command framework, the purgetags
    command inherits from a `NoArgsCommand`, which ensures that an error is
    raised if the user sends it parameters.

--- wtds/wallpapers/management/commands/purgetags.py
+++ wtds/wallpapers/management/commands/purgetags.py
@@ -1,10 +1,10 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import NoArgsCommand
 from ...models import Tag
 
-class Command(BaseCommand):
+class Command(NoArgsCommand):
     help = "Deletes all orphaned tags, belonging to no wallpapers"
 
-    def handle(self, *args, **options):
+    def handle_noargs(self, **options):
         orphans = Tag.objects.filter_orphaned()
         if orphans:
             self.stdout.write("Found {} orphaned tags.".format(orphans.count()))
