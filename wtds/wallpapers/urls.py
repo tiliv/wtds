55c510ba59305d9f19bcda6980d8e417fd3e51fc
Tim Valenta <tonightslastsong@gmail.com>
2013-02-16 00:25:01 -0700

    Flattens wallpaper urls
    Changes in the url structure should remove the possibility of searching a tag that collides with a built-in site url.

--- wtds/wallpapers/urls.py
+++ wtds/wallpapers/urls.py
@@ -5,14 +5,18 @@ from .views import (WallpaperCreateView, WallpaperUpdateView, WallpaperDetailVie
 
 urlpatterns = patterns('',
     url(r'^wallpapers/', include(patterns('',
-        url(r'^new/$', WallpaperCreateView.as_view(), name='upload'),
-        url(r'^list/', include(patterns('',
-            url(r'^$', WallpaperListView.as_view(), name='list'),
-            url(r'^(?P<slug>[\w-]+)/$', WallpaperListView.as_view(), name='list'),
-            url(r'^(?P<ratio>\d+:\d+)/$', WallpaperListView.as_view(), name='list'),
-            url(r'^in-danger/all/$', WallpaperListView.as_view(in_danger=True), name='in_danger'),
-            url(r'^in-danger/(?P<slug>[\w-]+)/$', WallpaperListView.as_view(in_danger=True), name='in_danger'),
+        # Filtered views
+        url(r'^$', WallpaperListView.as_view(), name='list'),
+        url(r'^tag/(?P<slug>[\w-]+)/$', WallpaperListView.as_view(), name='list'),
+        url(r'^(?P<ratio>\d+:\d+)/$', WallpaperListView.as_view(), name='list'),
+
+        # Special filtered views
+        url(r'^in-danger/', include(patterns('',
+            url(r'^$', WallpaperListView.as_view(in_danger=True), name='in_danger'),
+            url(r'^(?P<slug>[\w-]+)/$', WallpaperListView.as_view(in_danger=True), name='in_danger'),
         ))),
+
+        url(r'^new/$', WallpaperCreateView.as_view(), name='upload'),
         url(r'^(?P<pk>\d+)/', include(patterns('',
             url(r'^$', WallpaperDetailView.as_view(), name='view'),
             url(r'^download/$', WallpaperDetailView.as_view(download=True), name='download'),
@@ -20,10 +24,10 @@ urlpatterns = patterns('',
             url(r'^delete/$', WallpaperDeleteView.as_view(), name='delete'),
         ))),
     ), namespace='wallpapers')),
-    
+
     url(r'^tags/', include(patterns('',
         url(r'^$', TagListView.as_view(), name='list'),
-        url(r'^(?P<slug>[\w-]+)/', include(patterns('',
+        url(r'^inspect/(?P<slug>[\w-]+)/', include(patterns('',
             url(r'^edit/$', TagUpdateView.as_view(), name='edit'),
             url(r'^delete/$', TagDeleteView.as_view(), name='delete'),
         ))),
