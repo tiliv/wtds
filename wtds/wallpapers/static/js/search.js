ef4ffaa62d2f80e5c255d0732993350979bc6182
Tim Valenta <tonightslastsong@gmail.com>
2013-02-16 22:02:36 -0700

    Changes #search-bar to a form element

--- wtds/wallpapers/static/js/search.js
+++ wtds/wallpapers/static/js/search.js
@@ -1,11 +1,14 @@
 var search_input;
 
 function trigger_filter() {
-    search_input.trigger('change')
+    if (search_input) { // ignore the initial tagsinput construction
+        search_input.trigger('change');
+    }
 }
 
 $(function(){
     var search_bar = $('#search-bar');
+    var search_url = search_bar.attr('action');
     search_input = search_bar.find('input[name=terms]');
     search_bar.find('button[rel=clear]').on('click', function(){
         search_input.importTags('');
@@ -17,6 +20,6 @@ $(function(){
     search_input.on('change', function(){
         $(this).trigger('search.wtds');
     }).on('search.wtds', function(){
-        console.log("values:", $(this).val());
+        search_bar.submit();
     });
 });
