4bb64c0c5b7dfeda00ba4c4f061b3916a0c3b9d3
Tim Valenta <tonightslastsong@gmail.com>
2013-03-27 22:29:34 -0700

    Removes old PJAX middleware
    Implementation has moved to vanilla history.pushstate() manipulation.

--- wtds/core/middleware.py
+++
@@ -1,3 +0,0 @@
-class PjaxMiddleware(object):
-    def process_request(self, request):
-        request.is_pjax = lambda: request.META.get('HTTP_X_PJAX', False)
--- wtds/settings/base.py
+++ wtds/settings/base.py
@@ -84,8 +84,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'wtds.core.middleware.PjaxMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
