7246dc7d3623841451c8fdccdc3ca07fe0c36fba
Tim Valenta <tonightslastsong@gmail.com>
2013-04-25 00:51:20 -0700

    Adds proper status codes to special handlers

--- wtds/core/views.py
+++ wtds/core/views.py
@@ -17,15 +17,16 @@ class AuthenticationMixin(object):
                 return HttpResponseForbidden()
         return super(AuthenticationMixin, self).dispatch(request, *args, **kwargs)
 
-def _base_error(request, template_name=None, fake=False):
+def _base_error(request, status_code, template_name=None, fake=False):
     context = {'fake': fake}
     try:
         context = RequestContext(request, context)
-        print context
     except Exception as e:
         context = Context(context)
-    return render_to_response(template_name, context)
+    response = render_to_response(template_name, context)
+    response.status_code = status_code
+    return response
 
-handler403 = partial(_base_error, template_name='403.html')
-handler404 = partial(_base_error, template_name='404.html')
-handler500 = partial(_base_error, template_name='500.html')
+handler403 = partial(_base_error, status_code=403, template_name='403.html')
+handler404 = partial(_base_error, status_code=404, template_name='404.html')
+handler500 = partial(_base_error, status_code=500, template_name='500.html')
